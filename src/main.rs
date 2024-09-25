34b53d192074ae36b8260a7f7ff19827fd3c3c27
Angel <angelherrarte3@gmail.com>
2024-09-25 13:20:34 -0600

    Refactor main.rs to read config file and render scene

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use std::env;
+use std::fs;
+
+use rust_raytracing::config::Config;
+use rust_raytracing::raytracer::render;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    if args.len() != 3 {
+        println!("Usage: {} <config_file> <output_file>", args[0]);
+        return;
+    }
+
+    let json = fs::read(&args[1]).expect("Unable to read config file.");
+    let scene = serde_json::from_slice::<Config>(&json).expect("Unable to parse config json");
+
+    let filename = &args[2];
+    println!("\nRendering {}", filename);
+    render(&filename, scene);
 }
